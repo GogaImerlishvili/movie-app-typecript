2fb2fd739e6c1f3c76e0ec9d1f8d864598ec9f3b
GogaImerlishvili <gogaimerlishvili1@gmail.com>
2024-03-29 16:26:12 +0400

    add layout

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "../../Layout";
+import Sidebar from "../../components/sidebar";
+const Home = () => {
+  return (
+ 
+   <Layout>Home</Layout>
+ 
+
+  );
+};
+
+export default Home;
